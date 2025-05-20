05f77b2c63793d0a250a3abac920c376c2415c3e
hykim <joywithjoy1203@gmail.com>
2025-05-20 15:00:06 +0900

    chore: px to tailwind scale(or rem) (#5 #6)

--- src/app/result/components/Btn.tsx
+++ src/app/result/components/Btn.tsx
@@ -1,7 +1,9 @@
-export default function Btn({ type, text }: { type: number, text: string }) {
-   return (
-      <button className={`${type === 0 ? 'border border-[#02B694] text-[#02B694]' : 'text-white bg-[#02B694]'} flex-center md:w-[190px] w-full md:h-[52px] h-[34px] rounded-[9px]`}>
-         {text}
-      </button>
-   )
+export default function Btn({ type, text }: { type: number; text: string }) {
+  return (
+    <button
+      className={`${type === 0 ? 'border border-[#02B694] text-[#02B694]' : 'bg-[#02B694] text-white'} flex-center h-[2.125rem] w-full rounded-lg md:h-[2.125rem] md:w-[11.875rem]`}
+    >
+      {text}
+    </button>
+  );
 }
--- src/app/result/components/DetailInfo.tsx
+++ src/app/result/components/DetailInfo.tsx
@@ -1,21 +1,20 @@
 interface DetailInfoData {
-   title: string,
-   subTitle?: string,
-   desc: string,
-   studyMethod?: string,
+  title: string;
+  subTitle?: string;
+  desc: string;
+  studyMethod?: string;
 }
 
 export default function DetailInfo({ data }: { data: DetailInfoData }) {
-
-   return (
-      <div>
-         <strong className="md:text-[22px]">{data.title}</strong>
-         <div className="w-full md:text-[18px] rounded-[16px] md:p-7 p-5 mt-4 bg-[#2A374A]">
-            {data.subTitle &&
-               <p className="font-semibold md:mb-7 mb-5">{data.subTitle}</p>
-            }
-            <p>{data.desc}</p>
-         </div>
+  return (
+    <div>
+      <strong className="md:text-[1.375rem]">{data.title}</strong>
+      <div className="mt-4 w-full rounded-2xl bg-[#2A374A] p-5 md:p-7 md:text-[1.125rem]">
+        {data.subTitle && (
+          <p className="mb-5 font-semibold md:mb-7">{data.subTitle}</p>
+        )}
+        <p>{data.desc}</p>
       </div>
-   )
+    </div>
+  );
 }
