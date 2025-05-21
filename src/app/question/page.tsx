4e3d62293f6d43f4901594beff67a46b2e91bb0c
nijuy <youjin6325@naver.com>
2025-05-21 16:22:35 +0900

    feat: 설문 (/question) 페이지 컴포넌트 생성

---
+++ src/app/question/page.tsx
@@ -0,0 +1,15 @@
+export default function SurveyPage() {
+  return (
+    <div className="container flex h-dvh flex-col items-center justify-between md:justify-start">
+      <main className="w-full md:mb-[5.375rem]">
+        <div className="grid w-full gap-y-10 md:gap-y-[3.125rem]">
+          {/* <QuestionContent /> */}
+          {/* <RadioGroup /> */}
+        </div>
+      </main>
+      <nav className="mb-10 w-full md:mb-0 md:w-[30.5rem]">
+        {/* <ProgressBar /> */}
+      </nav>
+    </div>
+  );
+}
