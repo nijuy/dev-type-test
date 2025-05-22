149a04485edabb9eb9c6c8d0c63fd8ab33346487
nijuy <youjin6325@naver.com>
2025-05-22 16:37:54 +0900

    style: 컴포넌트 사이 여백 조정
    - 피그마대로 구현하느라 숫자가 지저분했던 부분을 임의 수정

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,8 +1,8 @@
 export default function QuestionPage() {
   return (
     <div className="container flex h-dvh flex-col items-center justify-between md:justify-start">
-      <main className="w-full md:mb-[5.375rem]">
-        <div className="grid w-full gap-y-10 md:gap-y-[3.125rem]">
+      <main className="w-full md:mb-20">
+        <div className="grid w-full gap-y-10">
           {/* <QuestionContent /> */}
           {/* <RadioGroup /> */}
         </div>
