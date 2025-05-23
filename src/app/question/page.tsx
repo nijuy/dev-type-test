078d41174772c6f91b06d9c074ca165f49e9b3a5
nijuy <youjin6325@naver.com>
2025-05-23 12:51:40 +0900

    style: 소형 기기 대응을 위한 레이아웃 수정
    - 소형 기기: iphone SE/8 이하의 11.9cm 디스플레이
    - 화면 스크롤을 위한 h-dvh ➡️ min-h-dvh
    - RadioGroup과 ProgressBar 사이 여백을 위한 mb-10 추가

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,7 +1,7 @@
 export default function QuestionPage() {
   return (
-    <div className="container flex h-dvh flex-col items-center justify-between md:justify-start">
-      <main className="w-full md:mb-20">
+    <div className="container flex min-h-dvh flex-col items-center justify-between md:justify-start">
+      <main className="mb-10 w-full md:mb-20">
         <div className="grid w-full gap-y-10">
           {/* <QuestionContent /> */}
           {/* <RadioGroup /> */}
