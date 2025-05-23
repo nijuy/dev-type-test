0baad7088ab217013846b046bc730ee7c1cf7049
usedupnote <leejm9704@naver.com>
2025-05-24 01:33:49 +0900

    feat: separate interface

--- src/app/question/components/QuestionContent.tsx
+++ src/app/question/components/QuestionContent.tsx
@@ -1,15 +1,10 @@
-interface QuestionContentData {
-  id: number;
-  text: string;
-  weight?: string[];
-  inverseWeight?: string[];
-}
+import { QuestionType } from '../types/QuestionContentData';
 
-export default function QuestionContent({ data }: { data: QuestionContentData }) {
+export default function QuestionContent({ data }: { data: QuestionType }) {
   return (
     <div className="w-full">
-      <p className="font-bold text-white mb-3.75 text-3xl md:mb-10 md:text-5xl">{`Q${data.id}.`}</p>
-      <p className="font-semibold text-white text-2xl md:text-4xl whitespace-pre-line">
+      <p className="mb-3.75 text-3xl font-bold text-white md:mb-10 md:text-5xl">{`Q${data.id}.`}</p>
+      <p className="text-2xl font-semibold whitespace-pre-line text-white md:text-4xl">
         {data.text}
       </p>
     </div>
