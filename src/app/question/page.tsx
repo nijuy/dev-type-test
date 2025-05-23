d252d663e939cee9a0ee7ab4a6c313cb085e2c98
이유진 <youjin6325@naver.com>
2025-05-23 13:59:04 +0900

    feat: 설문 페이지 컴포넌트 생성 (#13)
    * feat: 설문 (/question) 페이지 컴포넌트 생성
    
    * chore: `SurverPage` ➡️ `QuestionPage`
    
    * style: 컴포넌트 사이 여백 조정
    
    - 피그마대로 구현하느라 숫자가 지저분했던 부분을 임의 수정
    
    * style: 소형 기기 대응을 위한 레이아웃 수정
    
    - 소형 기기: iphone SE/8 이하의 11.9cm 디스플레이
    - 화면 스크롤을 위한 h-dvh ➡️ min-h-dvh
    - RadioGroup과 ProgressBar 사이 여백을 위한 mb-10 추가

---
+++ src/app/question/page.tsx
@@ -0,0 +1,15 @@
+export default function QuestionPage() {
+  return (
+    <div className="container flex min-h-dvh flex-col items-center justify-between md:justify-start">
+      <main className="mb-10 w-full md:mb-20">
+        <div className="grid w-full gap-y-10">
+          {/* <QuestionContent /> */}
+          {/* <RadioGroup /> */}
+        </div>
+      </main>
+      <nav className="mb-10 w-full md:mb-0 md:w-[30.5rem]">
+        {/* <ProgressBar /> */}
+      </nav>
+    </div>
+  );
+}
