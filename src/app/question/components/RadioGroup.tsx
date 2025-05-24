84a8205c72d2d4a0a2bcfdc8e67ac57e9035533f
usedupnote <leejm9704@naver.com>
2025-05-24 21:29:51 +0900

    feat: apply color tokens

--- src/app/question/components/RadioGroup.tsx
+++ src/app/question/components/RadioGroup.tsx
@@ -31,7 +31,7 @@ export default function RadioGroup({
             className="peer hidden w-full"
           />
           <div
-            className={`flex h-[3.13rem] w-full items-center truncate rounded-xl bg-[#F3F3F7] px-5 text-sm font-bold text-black transition-colors peer-checked:bg-[#14B8A6] peer-checked:text-white md:h-[3.75rem] md:px-7 md:text-lg`}
+            className={`peer-checked:bg-primary bg-foreground flex h-[3.13rem] w-full items-center truncate rounded-xl px-5 text-sm font-bold text-black transition-colors peer-checked:text-white md:h-[3.75rem] md:px-7 md:text-lg`}
           >
             {index + 1}. {label}
           </div>
