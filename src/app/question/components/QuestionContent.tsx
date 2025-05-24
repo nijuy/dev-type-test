82c105d1dcf0fdc5cd8a30bd5e38a0e5eea8ffea
usedupnote <leejm9704@naver.com>
2025-05-24 22:43:24 +0900

    refactor: QuestionContent props interface locally

--- src/app/question/components/QuestionContent.tsx
+++ src/app/question/components/QuestionContent.tsx
@@ -1,11 +1,14 @@
-import { QuestionType } from '../types/QuestionContentData';
+interface QuestionContentProps {
+  id: number;
+  text: string;
+}
 
-export default function QuestionContent({ data }: { data: QuestionType }) {
+export default function QuestionContent({ id, text }: QuestionContentProps) {
   return (
     <div className="w-full">
-      <p className="mb-3.75 text-3xl font-bold text-white md:mb-10 md:text-5xl">{`Q${data.id}.`}</p>
+      <p className="mb-3.75 text-3xl font-bold text-white md:mb-10 md:text-5xl">{`Q${id}.`}</p>
       <p className="text-2xl font-semibold whitespace-pre-line text-white md:text-4xl">
-        {data.text}
+        {text}
       </p>
     </div>
   );
