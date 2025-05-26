62b31f20caa8d717998e6e38b40dface2ceb134a
이유진 <youjin6325@naver.com>
2025-05-26 18:44:19 +0900

    feat: 설문 페이지 질문 간 화면 전환 로직 구현 (#26)
    * feat: 설문 진행 상태 관리를 위한 useQuestionProgress 훅 추가
    
    - 문항 간 이동과 응답 데이터 관리를 위한 타입 정의
    
    * feat: `/question` 페이지 컴포넌트에 useQuestionProgress 훅 적용

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,14 +1,44 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useQuestionProgress } from './hooks/useQuestionProgress';
+import QUESTIONS from '@/app/question/data/questions.json';
+import QuestionContent from './components/QuestionContent';
+import RadioGroup from './components/RadioGroup';
+import ProgressBar from './components/ProgressBar';
+
 export default function QuestionPage() {
+  const { currentIndex, answers, prev, next, select } = useQuestionProgress();
+  const question = QUESTIONS[currentIndex];
+
+  const router = useRouter();
+
+  const handleChange = (selected: number) => {
+    const isFirstAnswer = !answers[question.id];
+    select(question.id, selected);
+
+    const isLastQuestion = currentIndex === QUESTIONS.length - 1;
+    if (isLastQuestion) {
+      router.push('/result');
+    } else if (isFirstAnswer) {
+      setTimeout(next, 300);
+    }
+  };
+
   return (
     <div className="container flex min-h-dvh flex-col items-center justify-between md:justify-start">
       <main className="mb-10 w-full md:mb-20">
         <div className="grid w-full gap-y-10">
-          {/* <QuestionContent /> */}
-          {/* <RadioGroup /> */}
+          <QuestionContent text={question.question} id={question.id} />
+          <RadioGroup onChange={handleChange} active={answers[question.id]} />
         </div>
       </main>
       <nav className="mb-10 w-full md:mb-0 md:w-[30.5rem]">
-        {/* <ProgressBar /> */}
+        <ProgressBar
+          currentIndex={currentIndex}
+          hasAnswer={!!answers[question.id]}
+          clickPrev={prev}
+          clickNext={next}
+        />
       </nav>
     </div>
   );
