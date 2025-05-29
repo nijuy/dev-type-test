9e0261a3932887f2c844d6490c44c1ed89c5ae0f
usedupnote <leejm9704@naver.com>
2025-05-29 17:10:33 +0900

    feat: add opengraph image setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,12 @@ import './globals.css';
 export const metadata: Metadata = {
   title: '개발자 유형테스트',
   description: '어떤 개발자인지 확인해보세요!',
+  openGraph: {
+    type: 'website',
+    title: '개발자 유형 테스트',
+    description: '어떤 개발자인지 확인해보세요!',
+    siteName: '개발자 유형 테스트',
+  },
 };
 
 export default function RootLayout({
